a2b264394a4434dac96d7480c9d36d9d69f55eeb
bey8wie3pe <94910229+bey8wie3pe@users.noreply.github.com>
2023-05-22 17:42:16 +0900

    Add files via upload

--- routes/get.js
+++ routes/get.js
@@ -93,6 +93,16 @@ router.get('/login', (req, res) => {
 	res.render('login', { errorMessage: null, language: jsondata});
 });
 
+//ログアウト
+router.get('/logout', function (req, res) {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Error destroying session:', err);
+    }
+    res.redirect('/');
+  });
+});
+
 
 
 module.exports = router;
